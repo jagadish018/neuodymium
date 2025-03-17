f7f0dac890f0dc9a5594dd28234923b87f03b8a0
jagadishck018 <jagadishck.018@gmail.com>
2025-03-17 22:25:40 +0530

    refactor API calls in index.ts for better structure and clarity; streamline data fetching and logging

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,33 @@ import axios from "axios";
 const baseURL = "http://localhost:3000";
 
 try {
-   const response = await axios.get(`${baseURL}/generate`);
-   const randomNumber = Math.random() * 100;
-   console.log(`Random Number: ${randomNumber}`);
-   console.log(`API data:`, response.data);
-
-   const response1 = await axios.get(`${baseURL}/current-time`);
-   const date = new Date();
-   console.log("\n", date.toISOString());
-
-   const response2 = await axios.get(`${baseURL}/environment`);
-   const nodeVersion = process.version;
-   const platform = process.platform;
-   console.log(`Node.js version: ${nodeVersion}`);
-   console.log(`Platform: ${platform}`);
+  //generate random number
+  const reponse = await axios.get(`${baseURL}/generate`);
+  console.log(reponse.data);
 
 
+  // 2. GET /current-time
+  const currentTime = await axios.get(`${baseURL}/current-time`);
+  console.log(currentTime.data);
 
-  const response3 = await axios.get(`${baseURL}/puppet?k=8`);
-  const puppetVersion = response3.data;
-  console.log(puppetVersion);
+  // 3. GET /environment
+  const environment = await axios.get(`${baseURL}/environment`);
+  console.log(environment.data);
 
 
-  const response4 = await axios.post(`${baseURL}/numbers`, { number: 37 });
-  
-  const response5 = await axios.get(`${baseURL}/numbers`);
-  const numbers = response5.data;
-  console.log(numbers);
-  
-  
+  // 4. GET /puppet
+  const puppet = await axios.get(`${baseURL}/puppet?k=9`);
+  console.log(puppet.data);
+
+  // 5. POST /numbers
+  const numbers = await axios.post(`${baseURL}/numbers`, {number:8})
+
+
+  // 6. GET /numbers
+  const numbers2 = await axios.get(`${baseURL}/numbers`);
+  console.log(numbers2.data);
+
 
 } catch (error) {
   console.error(error);
-}
+  }
