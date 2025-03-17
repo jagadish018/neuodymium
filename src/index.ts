8a96f7cf8b1aad4fedc9985a8fe303058f9160e6
jagadishck018 <jagadishck.018@gmail.com>
2025-03-17 16:51:26 +0530

    refactor API calls in index.ts for improved readability and consistency

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,20 @@ import axios from "axios";
 const baseURL = "http://localhost:3000";
 
 try {
-  const response = await axios.get(`${baseURL}/generate`);
-  const randomNumber = Math.random() * 100;
-  console.log(`Random Number: ${randomNumber}`);
-  console.log(`API data:`, response.data);
+   const response = await axios.get(`${baseURL}/generate`);
+   const randomNumber = Math.random() * 100;
+   console.log(`Random Number: ${randomNumber}`);
+   console.log(`API data:`, response.data);
 
-  const response1 = await axios.get(`${baseURL}/current-time`);
-  const date = new Date();
-  console.log("\n", date.toISOString());
-  
+   const response1 = await axios.get(`${baseURL}/current-time`);
+   const date = new Date();
+   console.log("\n", date.toISOString());
 
-  const response2 = await axios.get(`${baseURL}/environment`);
-  const nodeVersion = process.version;
-  const platform = process.platform;
-  console.log(`Node.js version: ${nodeVersion}`);
-  console.log(`Platform: ${platform}`);
+   const response2 = await axios.get(`${baseURL}/environment`);
+   const nodeVersion = process.version;
+   const platform = process.platform;
+   console.log(`Node.js version: ${nodeVersion}`);
+   console.log(`Platform: ${platform}`);
 
 
 
@@ -25,8 +24,12 @@ try {
   const puppetVersion = response3.data;
   console.log(puppetVersion);
 
-  const numbers: number[] = [];
-  const response4 = await axios.post(`${baseURL}/numbers`);
+
+  const response4 = await axios.post(`${baseURL}/numbers`, { number: 37 });
+  
+  const response5 = await axios.get(`${baseURL}/numbers`);
+  const numbers = response5.data;
+  console.log(numbers);
   
   
 
